2b304e1298991938a40e3d5b31282778ec77fa23
JINAY113 <47896402+JINAY113@users.noreply.github.com>
2021-06-04 23:50:09 +0530

    Add files via upload

---
+++ DAY4/themepart/footer.php
@@ -0,0 +1,4 @@
+<div id="footer">
+	<p>2021. Website Manage By JINAY SHAH.</p>
+</div>
+
---
+++ DAY4/themepart/sidebar.php
@@ -0,0 +1,18 @@
+<div id="sidebar">
+			<ul>
+				
+				<li>
+					<h2>Skills</h2>
+					<ul>
+						<li><a href="#">HTML</a></li>
+						<li><a href="#">CSS</a></li>
+						<li><a href="#">C Programming</a></li>
+						<li><a href="#">Java</a></li>
+						<li><a href="#">PHP</a></li>
+						<li><a href="#">Python</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+		</div>
+
